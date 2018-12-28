ca8243528a05f7b5a612d9574189cb88d37ad3ae
maorroz <andory@post.bgu.ac.il>
2018-12-28 22:51:48 +0200

    added exprience

--- src/components/cv/body/exprience/Exprience.js
+++ src/components/cv/body/exprience/Exprience.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import './Exprience.css';
+import { positions } from './positions.json';
 import BodyHeader from '../BodyHeader';
 
+class Position extends Component {
+  render(){
+    const { name, field, title, startDate, endDate, description } = this.props.data;
+    const descriptionLines = description
+      .map((line, index) => <div dangerouslySetInnerHTML={{ __html: line }} key={index}/>);
+
+    return(
+      <div className='Position'>
+        <div className='Position-title'>{name} ({field}) - {title}</div>
+        <div className='Position-dates'>{startDate} - {endDate}</div>
+        {descriptionLines}
+
+
+      </div>
+    );
+  }
+}
+
 class Exprience extends Component {
+  positions(){
+    return positions.map((position, index) => <Position data={position} key={index}/>);
+  }
+
   render() {
     return (
       <div>
         <BodyHeader text='Work Exprience'/>
-          Exprience
+        {this.positions()}
       </div>
     );
   }
