b5408eb30c467a0f1dbfb38100151258b92c3426
maorroz <andory@post.bgu.ac.il>
2018-12-28 19:31:19 +0200

    added military section

--- src/components/cv/body/military/Military.js
+++ src/components/cv/body/military/Military.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import BodyHeader from '../BodyHeader';
+import './Military.css';
 
 class Military extends Component {
   render() {
     return (
       <div>
         <BodyHeader text='Military'/>
-          Military
+        <div className='Military-section'>
+          <span style={{ fontWeight: 'bold' }}>2010-2013: </span>
+          <span>Served as a fighter in the armored corps of IDF.</span>
+        </div>
       </div>
     );
   }
