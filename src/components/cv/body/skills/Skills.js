25517acfa1e8bcbb0a9e8bcb4d71909bcf4f4413
maorroz <andory@post.bgu.ac.il>
2018-12-29 00:56:04 +0200

    fixed the key issue in skills

--- src/components/cv/body/skills/Skills.js
+++ src/components/cv/body/skills/Skills.js
@@ -5,10 +5,18 @@ import { skills } from './skills.json';
 
 const MAX_STARS = 5;
 
+const createStarArray = (length, className) => {
+  const starArray = [];
+  for(let i = 0; i < length; i += 1){
+    starArray.push(<span className={className} key={i}>&#10026;</span>);
+  }
+  return starArray;
+};
+
 const SkillLine = (props) => {
   const { name, addition, level } = props.data;
-  const greenStars = Array(level).fill(<span className='Skill-green-star'>&#10026;</span>);
-  const greyStars = Array(MAX_STARS - level).fill(<span className='Skill-grey-star'>&#10026;</span>);
+  const greenStars = createStarArray(level, 'Skill-green-star');
+  const greyStars = createStarArray(MAX_STARS - level, 'Skill-grey-star');
   return(
     <div className='Skill-item'>
       {name} { addition ? `(${addition})` : ''}
